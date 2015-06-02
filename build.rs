b2ae70b4d80d7a048b935984e4b110cb9f13651b
Stephen Belanger <admin@stephenbelanger.com>
2015-06-02 16:13:16 -0700

    Got it working...though the linking process is kind of bad...

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
   Command::new("make").args(&["-f","makefile.cargo"]).status().unwrap();
 
   let out_dir = env::var("OUT_DIR").unwrap();
-  let v8_target = "x64.debug";
 
-  println!("cargo:rustc-flags=-L {}/{}", out_dir, v8_target);
+  println!("cargo:rustc-flags=-L {}", out_dir);
+  println!("cargo:rustc-link-search=native={}", out_dir);
 }
